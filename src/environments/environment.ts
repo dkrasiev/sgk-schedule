bdf761096537f09361442a327a78bd7397fbaa42
Дмитрий Красиев <dmitrykrasiev@gmail.com>
2022-04-05 13:11:52 +0400

    changed firebaseConfig

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,13 +5,13 @@
 export const environment = {
   production: false,
   firebaseConfig: {
-    apiKey: 'AIzaSyD4yQQ2ZGle0dDe7n_myMN9PGl7J6sEAr8',
-    authDomain: 'sgkshdedule.firebaseapp.com',
-    projectId: 'sgkshdedule',
-    storageBucket: 'sgkshdedule.appspot.com',
-    messagingSenderId: '759226125365',
-    appId: '1:759226125365:web:ac972ba06691858e4af57c',
-    measurementId: 'G-4ECX3C75TL',
+    apiKey: 'AIzaSyBaSwDFJbEpaDw6WMUf1El0hPk99dEDs7o',
+    authDomain: 'sgkschedule.firebaseapp.com',
+    projectId: 'sgkschedule',
+    storageBucket: 'sgkschedule.appspot.com',
+    messagingSenderId: '402095444962',
+    appId: '1:402095444962:web:5534efd6cee02aab3e5fbb',
+    measurementId: 'G-1LPLRC5NT5',
   },
 };
 
