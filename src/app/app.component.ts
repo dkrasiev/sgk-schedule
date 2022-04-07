4b005b225a152c24a4e472f9ed04b9e710d3a532
Дмитрий Красиев <dmitrykrasiev@gmail.com>
2022-04-07 17:12:35 +0400

    available update notification clean MaterialModule

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { SwUpdate } from '@angular/service-worker';
 import { Store } from '@ngrx/store';
 import { fadeInOut } from './animations/fadeInOut.animation';
 import * as LessonsActions from './lessons/store/lessons.actions';
@@ -11,9 +13,24 @@ import * as fromApp from './store/app.reducer';
   animations: [fadeInOut(300)],
 })
 export class AppComponent implements OnInit {
-  constructor(private store: Store<fromApp.AppState>) {}
+  constructor(
+    private store: Store<fromApp.AppState>,
+    private swUpdate: SwUpdate,
+    private snackBar: MatSnackBar
+  ) {}
 
   ngOnInit(): void {
     this.store.dispatch(new LessonsActions.LoadLastData());
+
+    this.swUpdate.checkForUpdate().then((updateAvailable) => {
+      if (updateAvailable) {
+        this.snackBar
+          .open('New update is available', 'Refresh page')
+          .onAction()
+          .subscribe(() => {
+            window.location.reload();
+          });
+      }
+    });
   }
 }
