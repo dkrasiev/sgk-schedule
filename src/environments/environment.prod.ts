7c64fea8abe041bbf860fe936f3442ad7b5c0159
Dmitry Krasiev <dmitrykrasiev@gmail.com>
2022-05-19 20:32:29 +0400

    remove firebase 2

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,3 @@
 export const environment = {
   production: true,
-  firebaseConfig: {
-    apiKey: 'AIzaSyBaSwDFJbEpaDw6WMUf1El0hPk99dEDs7o',
-    authDomain: 'sgkschedule.firebaseapp.com',
-    projectId: 'sgkschedule',
-    storageBucket: 'sgkschedule.appspot.com',
-    messagingSenderId: '402095444962',
-    appId: '1:402095444962:web:5534efd6cee02aab3e5fbb',
-    measurementId: 'G-1LPLRC5NT5',
-  },
 };
